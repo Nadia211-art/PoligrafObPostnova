928d7c4985d4722907cb6436b0d455a33f953dd0
dns <dns@LAPTOP-2UAMGV1U>
2025-11-12 09:10:39 +0300

    Добавьте файлы проекта.

---
+++ View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PoligrafObPostnova.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PoligrafObPostnova
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EquipmentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new EquipmentPage());
+        }
+
+        private void RentalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new RentalPage());
+        }
+
+        private void ClientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ClientsPage());
+        }
+    }
+}
